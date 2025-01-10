d0f815215223da99a7d1da11b2c89eeb55cf304f
Vedant469 <vedantnvidia4@gmail.com>
2025-01-10 12:56:47 +0530

    minor_change_for/mob

--- script.js
+++ script.js
@@ -1,20 +1,6 @@
-const toggleButton = document.getElementById("toggle-button");
-const navbarLinks = document.getElementById("navbar-links");
-const dropdown = document.querySelector('.dropdown'); 
-const dropdownContent = dropdown.querySelector('.dropdown-content');
+const toggleButton = document.getElementById('toggle-button');
+const navbarLinks = document.getElementById('navbar-links');
 
-toggleButton.addEventListener("click", function() {
-    navbarLinks.classList.toggle("active");
-    dropdownContent.classList.remove('show');
-});
-
-dropdown.addEventListener("click", function(event) {
-    event.stopPropagation();
-    dropdownContent.classList.toggle('show');
-});
-
-document.addEventListener("click", function(event) {
-    if (!dropdown.contains(event.target)) {
-        dropdownContent.classList.remove('show');
-    }
+toggleButton.addEventListener('click', () => {
+    navbarLinks.classList.toggle('active');
 });
