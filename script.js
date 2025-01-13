a4095d3d5b8ebb5d9ac15986b3b1724e61d797f1
Vedant469 <vedantnvidia4@gmail.com>
2025-01-14 02:29:58 +0530

    update

--- script.js
+++ script.js
@@ -7,18 +7,3 @@ toggleButton.addEventListener('click', () => {
    // Toggle the cross icon
    toggleButton.classList.toggle('active');
 });
-const excelFilePath = '"C:\Users\vedan\OneDrive\Desktop\ARV\data\data.xlsx"';
-
-app.post('/contact/data/excel', (req, res) => {
-    const data = req.body;
-    const query = 'INSERT INTO contact_data SET ?';
-    db.query(query, data, (err, results) => {
-        if (err) {
-            console.error('Error inserting data:', err);
-            res.status(500).send({ message: 'Error inserting data' });
-        } else {
-            console.log('Data inserted successfully:', results);
-            res.send({ message: 'Data inserted successfully' });
-        }
-    });
-});
